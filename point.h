dc5fd24ed5020a9ae29b95e691694b4c804d622f
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-04-14 22:10:44 +0530

    added include guard in point.h

--- point.h
+++ point.h
@@ -1,17 +1,5 @@
-/* 
-You are a single celled organism whose location is represented in two dimensions.
-You have several food targets around you, whose locations are also represented in two dimensions.
-
-Write a program, which when given the above data can determine the food target closest to the single celled organism.
-Assuming that there are five food targets at:
-    [18 76] [19 66] [89 57] [9 71] [55 38]
-
-Location of organism: [18 86],  Closest food target : [18 76]
-Location of organism: [97 27],  Closest food target : [89 57]
-Location of organism: [69 7],   Closest food target : [55 38]
-Location of organism: [10 94],  Closest food target : [18 76]
-Location of organism: [36 27],  Closest food target : [55 38]
-*/
+#ifndef __POINT_H_
+#define __POINT_H_
 
 #define DEFAULT_FOOD_LOCATIONS                        \
   {                                                   \
@@ -34,3 +22,5 @@ typedef struct
 double calculate_steps(Point, Point);
 
 void get_closest_food(Point *, int, Point, Point *);
+
+#endif
